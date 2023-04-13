8ec8e7c123712d2cc822d5b46ee3348cf50c344e
alikon <optimus4joomla@gmail.com>
2023-04-13 12:02:36 +0200

    clean

--- src/app.py
+++ src/app.py
@@ -65,8 +65,6 @@ with gallery_tab:
     st.write(gallery_files)
     gallery_dict = {image_path.split("/")[-1].split(".")[-2]: image_path
         for image_path in gallery_files}
-    st.write(gallery_dict)
-    st.write(gallery_dict.keys())
     options = list(gallery_dict.keys())
     file_name = st.selectbox("Select Art", 
                         options=options, index=options.index("demo"))
@@ -76,8 +74,6 @@ with gallery_tab:
         st.warning("To use the Gallery, remove the uploaded image first.")
     if st.session_state.get("image_url") not in ["", None]:
         st.warning("To use the Gallery, remove the image URL first.")
-    st.write(file[2:])
-    st.write(f"Current working dir: {os.getcwd()}")
     image = np.array(Image.open(file[2:]))
 
 with upload_tab:
