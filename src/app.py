e8facd45e3caace8b9b681148bfd5ed66ab501f6
alikon <optimus4joomla@gmail.com>
2023-04-13 10:48:32 +0200

    gallery

--- src/app.py
+++ src/app.py
@@ -61,7 +61,7 @@ st.write(*CLASSES)
 gallery_tab, upload_tab, url_tab = st.tabs(["Gallery", "Upload", "Image URL"])
 
 with gallery_tab:
-    gallery_files = glob(os.path.join(".", "./images", "*"))
+    gallery_files = glob(os.path.join(".", "gallery", "*"))
     gallery_dict = {image_path.split("/")[-1].split(".")[-2].replace("-", " "): image_path
         for image_path in gallery_files}
 
